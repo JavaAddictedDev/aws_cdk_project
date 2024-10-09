f956982797b1aa30963a4a424b625cb997c4dc60
Gabriel Valdivia <gabrielvaldivia.dev@outlook.com>
2024-10-09 19:00:58 -0300

    feat: add Service01Stack with Application Load Balanced Fargate Service
    - Implemented Service01Stack class to set up an Application Load Balanced Fargate service
    - Configured service with CPU, memory limits, and desired count
    - Integrated AWS CloudWatch logs for monitoring
    - Created a log group with a removal policy of DESTROY

---
+++ src/main/java/com/myorg/Service01Stack.java
@@ -0,0 +1,50 @@
+package com.myorg;
+
+import software.amazon.awscdk.RemovalPolicy;
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.logs.LogGroup;
+
+public class Service01Stack extends Stack {
+    
+    public Service01Stack(final Construct scope, final String id, Cluster cluster) {
+        this(scope, id, cluster, null);
+    }
+    
+    public Service01Stack(final Construct scope, final String id, Cluster cluster, final StackProps props) {
+        super(scope, id, props);
+        
+        ApplicationLoadBalancedFargateService service01 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB01").
+                serviceName("service-01")
+                .cluster(cluster)
+                .cpu(512)
+                .desiredCount(1)
+                .listenerPort(8889)
+                .memoryLimitMiB(1024)
+                .taskImageOptions(
+                        ApplicationLoadBalancedTaskImageOptions.builder()
+                                .containerName("aws_project_01")
+                                .image(ContainerImage.fromRegistry("siecola/curso_aws_project01:1.0.0"))
+                                .containerPort(8080)
+                                .logDriver(
+                                        LogDriver.awsLogs(
+                                                AwsLogDriverProps.builder()
+                                                        .logGroup(
+                                                                LogGroup.Builder.create(this, "Service01LogGroup")
+                                                                        .logGroupName("Service01")
+                                                                        .removalPolicy(RemovalPolicy.DESTROY)
+                                                                        .build())
+                                                        .streamPrefix("Service01")
+                                                        .build()))
+                                .build())
+                .publicLoadBalancer(Boolean.TRUE)
+                .build();
+    }
+}
