073b698643cd47f41aec019d6dfae0509062ebe1
Gabriel Valdivia <gabrielvaldivia.dev@outlook.com>
2024-10-09 12:34:12 -0300

    feat: add VpcStack class for creating a VPC with 3 AZs
    - Implemented VpcStack class extending Stack
    - Configured VPC with a maximum of 3 Availability Zones
    - Added VpcStack instantiation in the main application

---
+++ src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myorg;
+
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.constructs.Construct;
+
+/**
+ *
+ * @author rengoku
+ */
+public class VpcStack extends Stack {
+    
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+    
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+        
+        Vpc.Builder.create(this, "Vpc01")
+                .maxAzs(3)
+                .build();
+    }
+}
