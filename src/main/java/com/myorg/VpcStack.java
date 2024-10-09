28e0ac018db01e1723bc951461d12b090eb4f92e
Gabriel Valdivia <gabrielvaldivia.dev@outlook.com>
2024-10-09 15:58:08 -0300

    fix(vpc): set natGateways(0) to avoid unnecessary AWS charges

--- src/main/java/com/myorg/VpcStack.java
+++ src/main/java/com/myorg/VpcStack.java
@@ -23,7 +23,8 @@ public class VpcStack extends Stack {
         super(scope, id, props);
         
         Vpc.Builder.create(this, "Vpc01")
-                .maxAzs(3)
+                .maxAzs(2)
+                .natGateways(0)
                 .build();
     }
 }
