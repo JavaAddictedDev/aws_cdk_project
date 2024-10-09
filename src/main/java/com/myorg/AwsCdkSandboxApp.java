eb206f985244d133fffd31b333d7671756cce039
Gabriel Valdivia <gabrielvaldivia.dev@outlook.com>
2024-10-09 16:55:48 -0300

    refactor: enhance ClusterStack initialization with StackProps
    - Added StackProps to ClusterStack constructor for improved configuration
    - Simplified environment setup by creating a separate variable
    - Ensured consistent use of props across VpcStack and ClusterStack

--- src/main/java/com/myorg/AwsCdkSandboxApp.java
+++ src/main/java/com/myorg/AwsCdkSandboxApp.java
@@ -4,25 +4,25 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import software.amazon.awscdk.services.ec2.Vpc;
-
 public class AwsCdkSandboxApp {
 
     public static void main(final String[] args) {
         App app = new App();
 
-        StackProps vpcProps = StackProps.builder()
-                .env(Environment.builder()
-                        .region("us-east-2")
-                        .build())
+        Environment environment = Environment.builder()
+                .region("us-east-2")
                 .build();
-        
-        VpcStack vpcStack = new VpcStack(app, "Vpc", vpcProps);
-        
-        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getInstance());
-        
+
+        StackProps props = StackProps.builder()
+                .env(environment)
+                .build();
+
+        VpcStack vpcStack = new VpcStack(app, "Vpc", props);
+
+        ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getInstance(), props);
+
         clusterStack.addDependency(vpcStack);
-        
+
         app.synth();
     }
 }
